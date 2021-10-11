5669b4c4a83ed3324b7984d937cbe0d0385dcaff
CJBuchel <connorbuchel@gmail.com>
2021-10-12 01:50:16 +0800

    Updated run link

--- bootstrap/bootstrapLocal_mac.sh
+++ bootstrap/bootstrapLocal_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Run using curl -Ls https://github.com/wml-frc/CJ-Vision/blob/cj-vision-2.0/bootstrap/bootstrapLocal_mac.sh?raw=1 | bash
+# Run using /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/CJBuchel/robotSimFRC/master/bootstrap/bootstrapLocal_mac.sh)"
 
 
 echo Running mac bootstrap
