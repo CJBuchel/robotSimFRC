2714d5e4544095b94f3bc55037dd5c9006344e5d
CJBuchel <connorbuchel@gmail.com>
2021-10-12 18:49:04 +0800

    Added script for mac users (one time)

--- sim/macExportPath.sh
+++ sim/macExportPath.sh
@@ -3,5 +3,5 @@
 echo Exporting path
 export PKG_CONFIG_PATH="/usr/local/opt/opencv@4/lib/pkgconfig"
 export PKG_CONFIG_PATH="/usr/local/opt/opencv@/lib/pkgconfig"
-echo 'export PKG_CONFIG_PATH="/usr/local/opt/opencv@4/lib/pkgconfig"' > ~/.bashrc
-echo 'export PKG_CONFIG_PATH="/usr/local/opt/opencv@/lib/pkgconfig"' > ~/.bashrc
+echo 'export PKG_CONFIG_PATH="/usr/local/opt/opencv@4/lib/pkgconfig"' >> ~/.bashrc
+echo 'export PKG_CONFIG_PATH="/usr/local/opt/opencv@/lib/pkgconfig"' >> ~/.bashrc
