d7303d1554deaf35ec26445087b9b066c7c7e9e7
CJBuchel <connorbuchel@gmail.com>
2021-10-12 17:59:17 +0800

    Added export path for bootstrap script

---
+++ bootstrap/macExportPath.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Exporting path
+export PKG_CONFIG_PATH="/usr/local/opt/opencv@4/lib/pkgconfig"
+export PKG_CONFIG_PATH="/usr/local/opt/opencv@/lib/pkgconfig"
+echo 'export PKG_CONFIG_PATH="/usr/local/opt/opencv@4/lib/pkgconfig"' >> ~/.bash_profile
+echo 'export PKG_CONFIG_PATH="/usr/local/opt/opencv@/lib/pkgconfig"' >> ~/.bash_profile
