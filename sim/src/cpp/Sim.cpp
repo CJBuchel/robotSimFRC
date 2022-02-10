a7d3fd9762a539fa0c35ed56639ed5cfb07db352
CJBuchel <connorbuchel@gmail.com>
2022-02-11 02:21:07 +0800

    Trajectory update for acceleration and PID

--- sim/src/cpp/Sim.cpp
+++ sim/src/cpp/Sim.cpp
@@ -1,8 +1,8 @@
 #include "Sim.h"
 #include "robot/Drivetrain.h"
-#include "Trajectory.h"
+#include "TrajectoryUtilities.h"
 
-static constexpr double fieldRotation = 35.6;
+static constexpr double fieldRotation = 126.3;
 
 static double covnert2Meters(double rotations) {
   double rots2Meter = (fieldRotation/16);
@@ -21,12 +21,18 @@ Trajectory<Splines::CatmullRom> myTrajectory;
 void Sim::Init() {
   myTrajectory.push_back({
     {0,4}, {1,4}, {4,6}, {8,6}, {10,6}, {9,5},
-    {1,5}, {0,0}
-    // {0,4}, {1,4}, {2,4}, {3,4}
+    {1,4}, {0,0}
+    // {0,4}, {1,4}, {8,4}, {9,4}
   });
 
   myTrajectory.build();
 }
+
+PID::PIDGains travelGains{ "Traveling Gains", 0.1, 0, 0 };
+PID::PIDGains angleGains{ "Angling Gains", 0.17, 0, 0 };
+
+PID::PIDController travelController{ travelGains };
+PID::PIDController anglingController{ angleGains };
 
 /**
  * Periodic Update
@@ -34,36 +40,48 @@ void Sim::Init() {
 void Sim::Periodic() {
   double averageEncoder = (leftEnc.getRotations() + rightEnc.getRotations())/2;
   double distance = covnert2Meters(averageEncoder);
+  double robotGyro = World::getGyro(0); // input
+  
 
   double leftPower = 0;
   double rightPower = 0;
 
-  if (distance < myTrajectory.getRawTrajectory().totalLength) {
-    leftPower = 0.15;
-    rightPower = 0.15;
-    double robotGyro = World::getGyro(0); // input
-    double goalAngle = myTrajectory.getAngle(distance); // goal
+  // if (distance < myTrajectory.getRawTrajectory().totalLength) {
+  //   leftPower = 0.15;
+  //   rightPower = 0.15;
+  //   double robotGyro = World::getGyro(0); // input
+  //   double goalAngle = myTrajectory.getAngle(distance); // goal
 
-    if (myTrajectory.atWaypoint(1, distance)) {
-      std::cout << "At waypoint: " << 1 << std::endl;
-    }
+  //   /**
+  //    * Used for wrapping around a range. E.g, if current angle is 175 and goal is -180. Both 180 and -180 are the same.
+  //    * So don't spin the robot all the way around. instead just go to 180 degrees
+  //    */
+  //   double angleError = myTrajectory.wrap(goalAngle-robotGyro, 180); // (value = error, range = 180)
+  //   angleError /= 180;
 
-    /**
-     * Used for wrapping around a range. E.g, if current angle is 175 and goal is -180. Both 180 and -180 are the same.
-     * So don't spin the robot all the way around. instead just go to 180 degrees
-     */
-    double angleError = myTrajectory.wrap(goalAngle-robotGyro, 180); // (value = error, range = 180)
-    angleError /= 180;
+  //   leftPower += angleError;
+  //   rightPower -= angleError;
+  // }
 
-    leftPower += angleError;
-    rightPower -= angleError;
+
+  if (!myTrajectory.atWaypoint(myTrajectory.getRawTrajectory().points.size()-2, distance)) {
+    double targetDistance = myTrajectory.getRawTrajectory().totalLength; // total distance to travel/target
+    double targetAngle = myTrajectory.getAngle(distance);
+
+    travelController.setSetpoint(targetDistance);
+    anglingController.setSetpoint(targetAngle);
+    anglingController.setWrap(180);
+
+    double travelOutput = travelController.calculate(distance, Config::Sim::getGlobalDT());
+    double angleOutput = anglingController.calculate(robotGyro, Config::Sim::getGlobalDT());
+    leftPower = travelOutput + angleOutput;
+    rightPower = travelOutput - angleOutput;
+    std::cout << "Distance traveled: " << distance << ", Target Angle: " << targetAngle << ", Current Angle: " << robotGyro << std::endl;
   }
 
-
-
-
+  // std::cout << "Distance: " << averageEncoder << std::endl;
+  // leftPower = 0.15;
+  // rightPower = 0.15;
   left.set(leftPower);
   right.set(rightPower);
-
-  std::cout << distance << std::endl;
 }
